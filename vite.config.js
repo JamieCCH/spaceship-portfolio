029b13b1460af10c4dcd976b9dd0fa0d94904ef4
JamieCCH <jamie.cch@gmail.com>
2025-08-30 04:47:26 +0800

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/spaceship-portfolio/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true
-  },
-  server: {
-    host: true,
-    open: true
-  }
+  base: '/spaceship-portfolio/'
 })
