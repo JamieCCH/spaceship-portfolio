0f9c512f760a997701e640ef9142d23a31f4ca6f
JamieCCH <jamie.cch@gmail.com>
2025-09-09 23:40:59 +0800

    Avoid platforms overlapping with planets - Added collision detection with 100-unit minimum distance - Fallback safe zone placement if no valid position found - Removed empty directories

--- src/objects/Platforms.js
+++ src/objects/Platforms.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { PLATFORM_CONFIG } from '../utils/Constants.js'
+import { PLATFORM_CONFIG, PLANET_POSITIONS } from '../utils/Constants.js'
 
 export class Platforms {
     constructor(scene) {
@@ -26,17 +26,54 @@ export class Platforms {
             })
             const platform = new THREE.Mesh(platformGeometry, platformMaterial)
             
-            platform.position.set(
-                (Math.random() - 0.5) * PLATFORM_CONFIG.spreadRange,
-                -Math.random() * (PLATFORM_CONFIG.minY - PLATFORM_CONFIG.maxY) + PLATFORM_CONFIG.maxY,
-                (Math.random() - 0.5) * PLATFORM_CONFIG.spreadRange + 200
-            )
+            // Generate position that doesn't overlap with planets
+            let position
+            let attempts = 0
+            const maxAttempts = 50
+            
+            do {
+                position = {
+                    x: (Math.random() - 0.5) * PLATFORM_CONFIG.spreadRange,
+                    y: -Math.random() * (PLATFORM_CONFIG.minY - PLATFORM_CONFIG.maxY) + PLATFORM_CONFIG.maxY,
+                    z: (Math.random() - 0.5) * PLATFORM_CONFIG.spreadRange
+                }
+                attempts++
+            } while (this.isOverlappingWithPlanets(position) && attempts < maxAttempts)
+            
+            // If we couldn't find a good position after many attempts, 
+            // place it in a safe zone far from planets
+            if (attempts >= maxAttempts) {
+                position = {
+                    x: (Math.random() - 0.5) * 200 - 400, // Far to the left
+                    y: -Math.random() * (PLATFORM_CONFIG.minY - PLATFORM_CONFIG.maxY) + PLATFORM_CONFIG.maxY,
+                    z: (Math.random() - 0.5) * 200
+                }
+            }
+            
+            platform.position.set(position.x, position.y, position.z)
             platform.castShadow = true
             platform.receiveShadow = true
             
             this.platforms.push(platform)
             this.scene.addObject(platform)
         }
+    }
+
+    isOverlappingWithPlanets(position) {
+        const minDistance = 100 // Minimum distance from planet center
+        
+        for (const planetPos of PLANET_POSITIONS) {
+            const distance = Math.sqrt(
+                Math.pow(position.x - planetPos.x, 2) +
+                Math.pow(position.z - planetPos.z, 2)
+            )
+            
+            if (distance < minDistance) {
+                return true
+            }
+        }
+        
+        return false
     }
 
     getPlatforms() {
